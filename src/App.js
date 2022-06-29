89bcdfadb82b9256221f7c770b6fda670f2e6b6e
DdooBuggy <yhiyo03@gmail.com>
2022-06-29 22:26:31 +0900

    created useInput

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import { useState } from "react";
 import "./App.css";
 
-const App = () => {
-  const [count, setCount] = useState(0);
-  const [email, setEmail] = useState("");
-  const updateEmail = (e) => {
-    const value = e.target.value;
-    setEmail(value);
+const useInput = (initialValue, validator) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = (e) => {
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(e.target.value);
+    }
+    if (willUpdate) {
+      setValue(e.target.value);
+    }
   };
+  return { value, onChange };
+};
+
+const App = () => {
+  const maxLen = (value) => value.length <= 10;
+  const name = useInput("Mr.", maxLen);
   return (
     <>
-      <div>{count}</div>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-      <input placeholder="Email" value={email} onChange={updateEmail} />
+      <h1>Hello</h1>
+      <input placeholder="Name" {...name} />
     </>
   );
 };
