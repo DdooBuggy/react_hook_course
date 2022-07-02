8653bcf5054de23c16924252ffe3ba6da1655a8c
DdooBuggy <yhiyo03@gmail.com>
2022-07-02 17:27:30 +0900

    ~#2.8

--- src/App.js
+++ src/App.js
@@ -1,33 +1,48 @@
 import { useEffect, useRef, useState } from "react";
+import defaultAxios from "axios";
 import "./App.css";
 
-const useNotification = (title, options) => {
-  if (!("Notification" in window)) {
-    return;
-  }
-  const fireNotification = () => {
-    if (Notification.permission !== "granted") {
-      Notification.requestPermission().then((permission) => {
-        if (permission === "granted") {
-          new Notification(title, options);
-        } else {
-          return;
-        }
-      });
-    } else {
-      new Notification(title, options);
-    }
+const useAxios = (options, axiosInstance = defaultAxios) => {
+  const [state, setState] = useState({
+    loading: true,
+    error: null,
+    data: null,
+  });
+  const [trigger, setTrigger] = useState(0);
+  const refetch = () => {
+    setState({
+      ...state,
+      loading: true,
+    });
+    setTrigger(Date.now());
   };
-  return fireNotification;
+  useEffect(() => {
+    if (!options.url) {
+      return;
+    }
+    axiosInstance(options)
+      .then((data) => setState({ ...state, loading: false, data }))
+      .catch((error) => setState({ ...state, loading: false, error }));
+  }, [trigger]);
+  return { ...state, refetch };
 };
 
 const App = () => {
-  const triggerNotification = useNotification("Do you need any help?", {
-    body: "Yes",
+  const { loading, data, error, refetch } = useAxios({
+    url: "https://yts.mx/api/v2/list_movies.json",
   });
+  console.log(data);
   return (
     <div className="App">
-      <button onClick={triggerNotification}>Btn</button>
+      <button onClick={refetch}>Refetch</button>
+      <h3>{loading ? "Loading..." : "Finished"}</h3>
+      {data && (
+        <ul>
+          {data.data.data.movies.map((movie) => (
+            <li key={movie.id}>{movie.title}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
